14ec78996918f32b104b0352f3fdbfe52264a438
胡宏韬 <3183764662@qq.com>
2018-11-14 11:35:30 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # in-ouath2
 Demo tutorial on spring boot integration with OAUTH2, which will eventually build a OAuth2 server（关于Spring Boot与OAuth2集成的Demo教程，最终将构建一个OAuth2服务器）
+
+
+## v0.0.1
+
+登录跳转Facebook登录界面
+
+
--- src/main/java/com/myself/oauth/InOauth2Application.java
+++ src/main/java/com/myself/oauth/InOauth2Application.java
@@ -2,8 +2,10 @@ package com.myself.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class InOauth2Application {
 
 	public static void main(String[] args) {
