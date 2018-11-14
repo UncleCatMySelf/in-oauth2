231d53a7649ef1856ab961cb71585ba2f36ab4a1
胡宏韬 <3183764662@qq.com>
2018-11-14 11:19:05 +0800

    init,project

---
+++ README.md
@@ -0,0 +1,2 @@
+# in-ouath2
+Demo tutorial on spring boot integration with OAUTH2, which will eventually build a OAuth2 server（关于Spring Boot与OAuth2集成的Demo教程，最终将构建一个OAuth2服务器）
---
+++ src/main/java/com/myself/oauth/InOauth2Application.java
@@ -0,0 +1,12 @@
+package com.myself.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InOauth2Application.class, args);
+	}
+}
