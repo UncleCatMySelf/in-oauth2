d8edfdb7dd9dc57201a35c8237e12c7c94d47b35
胡宏韬 <3183764662@qq.com>
2018-11-14 15:12:14 +0800

    completion

--- client/src/main/java/com/myself/oauth/ClientApplication.java
+++ client/src/main/java/com/myself/oauth/ClientApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @Configuration
@@ -17,10 +22,13 @@ public class ClientApplication {
 
 	@RequestMapping("/")
 	public String home(Principal user) {
+		System.out.println("Hello " + user.getName());
 		return "Hello " + user.getName();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
+
+
 }
--- src/main/java/com/myself/oauth/InOauth2Application.java
+++ src/main/java/com/myself/oauth/InOauth2Application.java
@@ -3,6 +3,7 @@ package com.myself.oauth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
@@ -10,6 +11,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
@@ -19,6 +22,8 @@ import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilt
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -29,20 +34,30 @@ import org.springframework.web.filter.CompositeFilter;
 import javax.servlet.Filter;
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableOAuth2Client
 @RestController
 @EnableAuthorizationServer
+@Order(SecurityProperties.BASIC_AUTH_ORDER)
 public class InOauth2Application extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	OAuth2ClientContext oauth2ClientContext;
 
-	@RequestMapping("/user")
-	public Principal user(Principal principal){
-		return principal;
+//	@RequestMapping("/user")
+//	public Principal user(Principal principal){
+//		return principal;
+//	}
+
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		return map;
 	}
 
 	@Bean
@@ -106,6 +121,17 @@ public class InOauth2Application extends WebSecurityConfigurerAdapter {
 		SpringApplication.run(InOauth2Application.class, args);
 	}
 
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration
+			extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http
+					.antMatcher("/me")
+					.authorizeRequests().anyRequest().authenticated();
+		}
+	}
 
 	class ClientResources {
 
